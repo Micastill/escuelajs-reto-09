d02337924450c168b512787812e3e1d62928f1b1
Miguel castillo <mcastillo@360pmi.com>
2019-10-26 11:48:49 -0500

    reto parte 1 y 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,9 +19,58 @@ const platziStore = (app) => {
   });
 
   router.get('/products', async (req, res, next) => {
-    const storeProducts = await productService.getProducts()
-    res.status(200).json(storeProducts);
+    try {
+      const storeProducts = await productService.getProducts()
+      res.status(200).json({data: storeProducts, message: 'Products'});
+    } catch (error) {
+      console.log(error);
+      
+    }
+  
   });
+
+  router.get('/:productId', async (req, res, next) => {
+    const { productId } = req.params;
+    try {
+      const storeProducts = await productService.getProduct(productId);
+      res.status(200).json({data: storeProducts, message: 'Product details'});
+      
+    } catch (error) {
+      console.log(error)
+    }
+    
+  });
+
+  router.put('/:productId', async (req, res, next) =>{
+    const { productId } = req.params;
+    const { body: data } = req;
+    try {
+      const storeProducts = await productService.updateProduct(productId, data);
+      res.status(200).json({data: storeProducts, message: 'Product updated'})
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  router.delete('/:productId', async (req, res, next) =>{
+    const { productId } = req.params;
+    try {
+      const storeProducts = await productService.deleteProduct(productId);
+      res.status(200).json({data: storeProducts, message: 'Product deleted'})
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  router.post('/products', async (req, res, next) =>{
+    const { body: data } = req
+    try {
+      const storeProducts = await productService.createOne(data);
+      res.status(201).json({data: storeProducts, message: 'Product created'})
+    } catch (error) {
+      
+    }
+  })
 
   router.get('*', (req, res) => {
     res.status(404).send('Error 404');
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,42 @@
-const { productsMock } = require('../utils/mocks');
+//const { productsMock } = require('../utils/mocks');
+const MongoConnect = require('../lib/mongo');
+const { ObjectId } = require ('mongodb')
 
 class ProductService {
+  constructor(){
+    this.collection = 'products'
+    this.mongoDB = new MongoConnect();
+  }
+
   async getProducts() {
-    const products = await Promise.resolve(productsMock);
+    const query = {}
+    const options = {}
+    const products = await this.mongoDB.get(this.collection, query, options);
     return products || [];
+  }
+
+  async getProduct(productId){
+    const query = {_id : ObjectId(productId)}
+    const options = {}
+    const product = await this.mongoDB.getOne(this.collection, query, options);
+    return product;
+  }
+
+  async updateProduct(productId, data){
+    const query ={_id : ObjectId(productId)};
+    const updateId = await this.mongoDB.update(this.collection, query, data)
+    return updateId;
+  }
+
+  async deleteProduct (productId){
+    const query = {_id : ObjectId(productId)};
+    const deleteId = await this.mongoDB.delete(this.collection, query)
+    return deleteId;
+  }
+
+  async createOne (data){
+    const createdId = await this.mongoDB.create(this.collection, data)
+    return createdId;
   }
 }
 
